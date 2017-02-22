01ecada557c210de424b2f1c5d9929e5f2c14538
diipkumar <dilipkumar@hakunamatata.in>
2017-02-22 10:39:44 +0530

    rest example

---
+++ RestIndex.js
@@ -0,0 +1,29 @@
+/**
+ * Created by dilip on 17/2/17.
+ */
+"use strict";
+
+var routes = require('./lib/routes');
+const appConfig = require('./lib/config/app');
+
+
+const Hapi = require('hapi');
+
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: 4041
+});
+
+// Add the route
+server.route(routes);
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
